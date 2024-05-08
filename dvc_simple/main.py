96a945a28e5bb4a54bfd3e8fb18a44f4549fd953
alekseik1 <1alekseik1@gmail.com>
2024-05-08 19:34:37 +0300

    print paths

--- dvc_simple/main.py
+++ dvc_simple/main.py
@@ -1,3 +1,11 @@
+import sys
+import importlib
+from importlib.util import find_spec
+
+print(sys.path)
+print(find_spec("dvc_simple.deep.dep").origin)
+print(find_spec("deep.dep").origin)
+
 from dvc_simple.deep import dep
 
 print(dep.my_str)
